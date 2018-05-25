a995db0679a94cb65fc9fb265b5983680a34b3df
Seth Coussens <seth.coussens@gmail.com>
2018-05-24 23:18:22 -0500

    changes to navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxElectronModule } from 'ngx-electron';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './component/home/home.component';
 import { AppRoutingModule } from './modules/routing.module';
 import { PowershellService } from './services/powershell.service';
 import { GridModule } from '@progress/kendo-angular-grid';
 import { LayoutModule } from '@progress/kendo-angular-layout';
+import { NavComponent } from './component/nav/nav.component';
+import { WelcomeComponent } from './component/welcome/welcome.component';
+import { LicenseComponent } from './component/license/license.component';
+import { ConfigurationComponent } from './component/configuration/configuration.component';
+import { ConfirmationComponent } from './component/confirmation/confirmation.component';
+import { InstallationComponent } from './component/installation/installation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent,
+    WelcomeComponent,
+    LicenseComponent,
+    ConfigurationComponent,
+    ConfirmationComponent,
+    InstallationComponent
   ],
   imports: [
     BrowserAnimationsModule,
--- src/app/modules/routing.module.ts
+++ src/app/modules/routing.module.ts
@@ -1,11 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from '../components/home/home.component';
+import { HomeComponent } from '../component/home/home.component';
+import { WelcomeComponent } from '../component/welcome/welcome.component';
+import { LicenseComponent } from '../component/license/license.component';
+import { ConfigurationComponent } from '../component/configuration/configuration.component';
+import { ConfirmationComponent } from '../component/confirmation/confirmation.component';
+import { InstallationComponent } from '../component/installation/installation.component';
 
 const routes: Routes = [
     {
         path: '',
-        component: HomeComponent
+        component: WelcomeComponent
+    },
+    {
+        path: 'welcome',
+        component: WelcomeComponent
+    },
+    {
+        path: 'license',
+        component: LicenseComponent
+    },
+    {
+        path: 'configuration',
+        component: ConfigurationComponent
+    },
+    {
+        path: 'confirmation',
+        component: ConfirmationComponent
+    },
+    {
+        path: 'installation',
+        component: InstallationComponent
     }
 ];
 
